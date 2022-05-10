c8321fbd719b6dd1c0a84c9bf09ff0dde7c1de3d
tec <76844890+tecnayn@users.noreply.github.com>
2022-05-10 19:07:52 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    app: {
+        px: 'x.',
+        token: 'XXX',
+        playing: 'by narchoticnoriane#2009 ❤️'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
